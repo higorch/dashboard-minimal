170b451677a7870ca8208efff891b27d0f54d63c
higorch <higorchristianfe@gmail.com>
2021-10-28 13:46:16 -0300

    important, fix error _attachPositioningHandler select2 in modal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,34 @@
 (function ($) {
 
+    // important, fix error "_attachPositioningHandler" select2 in modal
+    if ($.fn.select2) {
+        $.fn.select2.amd.require(["select2/dropdown/attachBody", "select2/utils"], (AttachBody, Utils) => {
+            AttachBody.prototype._attachPositioningHandler = function (decorated, container) {
+                var self = this;
+                var scrollEvent = "scroll.select2." + container.id;
+                var resizeEvent = "resize.select2." + container.id;
+                var orientationEvent = "orientationchange.select2." + container.id;
+                var $watchers = this.$container.parents().filter(Utils.hasScroll);
+                $watchers.each(function () {
+                    $(this).data("select2-scroll-position", {
+                        x: $(this).scrollLeft(),
+                        y: $(this).scrollTop()
+                    });
+                });
+                $watchers.on(scrollEvent, function (ev) {
+                    var position = $(this).data("select2-scroll-position");
+                    $(self).scrollTop(position.y); // patch: this => self
+                });
+                $(window).on(scrollEvent + " " + resizeEvent + " " + orientationEvent,
+                    function (e) {
+                        self._positionDropdown();
+                        self._resizeDropdown();
+                    }
+                );
+            };
+        });
+    }
+
+    // below starts your code 
+
 })(jQuery);
