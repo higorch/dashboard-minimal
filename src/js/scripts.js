172d4a34295e2454d8fbd399020f6084c5a5f7c1
Higor Christian <higorchristianfe@gmail.com>
2022-01-24 16:23:22 -0300

    file upload preview

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -32,6 +32,107 @@
         }
     }
 
+    /**
+     * Multiple files updalod preview
+     */
+    $.fn.attachFiles = function () {
+
+        var input = $(this);
+
+        $(input).on('change', function (e) {
+
+            var el = $(this);
+            var box_files = el.parents('.box-attach').next('.box-files');
+
+            if (el[0].files.length > 0) {
+
+                box_files.addClass('active');
+
+                var output = '<ul>';
+
+                $.each(el[0].files, function (index, file) {
+
+                    var src = URL.createObjectURL(file);
+
+                    output += '<li>';
+
+                    output += '<div class="box">';
+
+                    // preview thumb
+                    switch (file.type) {
+                        case 'image/apng':
+                        case 'image/avif':
+                        case 'image/gif':
+                        case 'image/jpeg':
+                        case 'image/png':
+                        case 'image/svg+xml':
+                        case 'image/webp':
+                        case 'image/x-icon':
+                            output += '<div class="preview"><img src="' + src + '"></div>';
+                            break;
+
+                        case 'text/csv':
+                            output += '<div class="preview"><div class="icon"><i class="fas fa-file-csv"></i></div></div>';
+                            break;
+
+                        case 'application/pdf':
+                            output += '<div class="preview"><div class="icon"><i class="fas fa-file-pdf"></i></div></div>';
+                            break;
+
+                        default:
+                            output += '<div class="preview"><div class="icon"><i class="far fa-file-alt"></i></div></div>';
+                            break;
+                    }
+
+                    output += '<div class="details">';
+                    output += '<div class="info">';
+                    output += '<span class="name">' + file.name + '</span>';
+                    output += '<span class="progress">0%</span>';
+                    output += '</div>';
+                    output += '<div class="progressbar"><span class="blue" style="width: 0%;"></span></div>';
+                    output += '</div>';
+
+                    output += '<div class="actions">';
+
+                    // preview btn
+                    switch (file.type) {
+                        case 'image/apng':
+                        case 'image/avif':
+                        case 'image/gif':
+                        case 'image/jpeg':
+                        case 'image/png':
+                        case 'image/svg+xml':
+                        case 'image/webp':
+                        case 'image/x-icon':
+                        case 'application/pdf':
+                            output += '<a href="' + src + '" class="external-link btn btn-dark-gray" title="Visualizar" target="_blank"><i class="fas fa-external-link-alt"></i></a>';
+                            break;
+                    }
+
+                    output += '<a href="#" class="delete btn btn-black" title="Excluir"><i class="fas fa-times"></i></a>';
+                    output += '</div>';
+
+                    output += '</div>';
+
+                    output += '</li>';
+
+                    console.log();
+
+                });
+
+                output += '</ul>';
+
+                box_files.html(output);
+
+            } else {
+                box_files.removeClass('active');
+            }
+        });
+
+    }
+
+    $("#myfiles").attachFiles();
+
     $('.box-catalog.inline >').matchHeight();
     $('.scrollbar-macosx').scrollbar();
 
